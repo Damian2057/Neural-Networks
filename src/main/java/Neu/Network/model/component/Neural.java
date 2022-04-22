ffdf3d6ca5b1beaa4b917ecd59f1bbf65f295ab9
Damiaan <ddamian2057@gmail.com>
2022-04-22 14:08:00 +0200

    project structure init

---
+++ src/main/java/Neu/Network/model/component/Neural.java
@@ -0,0 +1,4 @@
+package Neu.Network.model.component;
+
+public class Neural {
+}
---
+++ src/main/java/Neu/Network/userCommunication/App.java
@@ -0,0 +1,7 @@
+package Neu.Network.userCommunication;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
